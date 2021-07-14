511877064e7fe3d0ad9faf6c650f1061c5a2ec57
Agustin Barajas <agustinbvaldivia@gmail.com>
2021-07-13 18:10:06 -0600

    Add keys from data file

--- drum-kit/index.js
+++ drum-kit/index.js
@@ -1,3 +1,20 @@
+(async function () {
+    const keysContainer = document.querySelector('.keys-container');
+    const res = await fetch('./assets/data/data.json');
+    const data = await res.json();
+
+    keysContainer.innerHTML = data.map(({ key, kbd, sound }) => {
+        return `<div data-key="${key}"class="key">
+            <kbd>${kbd}</kbd>
+            <span class="sound">${sound}</span>
+        </div>`;
+    }).join('');
+
+    const keys = Array.from(document.querySelectorAll('.key'));
+    keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+    window.addEventListener('keydown', playSound);
+})();
+
 function removeTransition(evt) {
     if (evt.propertyName !== 'transform') {
         return;
@@ -17,7 +34,3 @@ function playSound(evt) {
     audio.currentTime = 0;
     audio.play();
 }
-
-const keys = Array.from(document.querySelectorAll('.key'));
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-window.addEventListener('keydown', playSound);
