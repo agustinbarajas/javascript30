629085b4758b75094ac610e0cfa5cfe8b3b167e4
Agustin Barajas <agustinbvaldivia@gmail.com>
2021-07-15 21:14:42 -0600

    Add enhancements to image and controls

--- css-variables/index.js
+++ css-variables/index.js
@@ -1,9 +1,11 @@
-const inputs = document.querySelectorAll('.controls input');
+const inputs = document.querySelectorAll('.control input');
 
 function handleUpdate() {
     const suffix = this.dataset.sizing || '';
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
-inputs.forEach(input => input.addEventListener('change', handleUpdate));
-inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+inputs.forEach(input => {
+    input.addEventListener('change', handleUpdate);
+    input.addEventListener('mousemove', handleUpdate);
+});
